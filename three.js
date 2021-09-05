b19c7b681fc8ea82d0641e4877ab75d8dfe11e8f
bentoBAUX <bennettpoh@gmail.com>
2021-09-05 16:25:43 +0800

    Second Commit
    Changed title
    Edited code

--- three.js
+++ three.js
@@ -1,3 +1,4 @@
+//scene setup
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
@@ -19,10 +20,10 @@ window.addEventListener('resize', () => {
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
-
+//mesh setup
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var wireframe = new THREE.EdgesGeometry(geometry);
-var material = new THREE.LineBasicMaterial({ color: 0x7d7d7d });
+var material = new THREE.LineBasicMaterial({ color: 0x333333 });
 
 var center_mat = new THREE.LineBasicMaterial({ color: 0xffa1a1 });
 var center_line = new THREE.LineSegments(wireframe, center_mat);
@@ -37,8 +38,13 @@ scene.add(center_line);
 
 
 meshX = -10;
+
+//spawning mesh
 for (var i = 0; i < 25; i++) {
     var line = new THREE.LineSegments(wireframe, material);
+    line.material.depthTest = false;
+    line.material.opacity = 0.25;
+    line.material.transparent = true;
     line.position.x = (Math.random() - 0.5) * 10;
     line.position.y = (Math.random() - 0.5) * 10;
     line.position.z = (Math.random() - 0.5) * 10;
@@ -46,7 +52,7 @@ for (var i = 0; i < 25; i++) {
     meshX += 1;
 }
 
-
+//ligh setup
 var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
 light.position.set(0, 0, 0);
 scene.add(light);
@@ -60,6 +66,7 @@ var render = function () {
     renderer.render(scene, camera);
 }
 
+//animation trigger
 function onMouseMove(event) {
     event.preventDefault();
 
@@ -80,6 +87,5 @@ function onMouseMove(event) {
 
 
 render();
-
-
 window.addEventListener('mousemove', onMouseMove);
+
